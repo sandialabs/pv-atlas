1c26360a04461f5c106b2be1dcba836124f0c97e
Kevin Anderson <kevin.anderso@gmail.com>
2024-08-13 09:41:38 -0400

    add initial website content

---
+++ website/source/chapter/climatic-influence-plr.rst
@@ -0,0 +1,132 @@
+
+.. map-header::
+
+
+Climatic influence on PLR uncertainty
+=====================================
+
+
+Introduction
+------------
+
+PV systems experience gradual performance reduction over their lifetime due to component
+aging and other effects.  Many technology- and climate-specific mechanisms
+contribute to this performance loss, making it difficult to simulate and predict from
+first principles.  Instead, it is common to extract empirical performance loss rates (PLR)
+from performance measurements from fielded systems using statistical techniques.
+
+Many statistical techniques for PLR estimation have been proposed.  What has
+emerged as a de facto standard is the "year-on-year" (YOY) method as implemented
+in `RdTools <https://github.com/NREL/rdtools>`_.  From a multi-year production
+timeseries dataset, the YOY method extracts an estimated PLR and associated
+confidence interval using a sequence of data filtering, normalization, aggregation,
+and bootstrapping steps.  The accuracy and uncertainty of the PLR estimate
+depend on the ability of these data processing steps to suppress the "noise"
+introduced by effects like resource variability and identify the underlying
+performance trend.  The longer the dataset being analyzed, and the more stable the
+climate, the more certain the PLR estimate becomes.
+
+Since these estimated PLRs affect the financing of new PV systems, understanding
+the uncertainty of these estimates brings significant value.
+This chapter examines the YOY method's performance across climates, with the central
+question: **how many years of data are needed to achieve a given level of certainty,
+and how does it vary across climates?**
+
+Methodology
+-----------
+
+Synthetic PV performance datasets based on NSRDB PSM3 data and an assumed
+PLR of -0.75 %/year were simulated for a grid of locations across the United
+States.  The datasets vary in length from 2-10+ years, and are repeated in
+each location to span the full history of the PSM3 dataset so that long-term
+resource variability is captured.  The year-on-year method is then applied
+to each dataset and the 95% confidence interval for the estimated PLR
+is recorded.  This gives a set of confidence intervals for each location and
+dataset length.  These confidence intervals are then examined to determine,
+for each location, how many years of data are required to achieve a given
+level of certainty in the PLR estimate.  This process can be repeated
+for different simulation parameters (e.g. PV technology).
+
+Using these idealized simulated datasets, we can produce a "best case"
+estimate for PLR estimation uncertainty.  In real datasets, the true uncertainty
+will be larger due to other sources of noise like system outages, sensor error,
+and array soiling.
+
+For full details on the simulation and analysis methodology,
+see :cite:`Theristis2023srrl`.
+
+
+Scenario 1: c-Si
+----------------
+
+First, we examine the results for a simulated system using crystalline silicon
+(c-Si) PV modules.
+
+This map visualizes the minimum dataset length (in years) for the PLR
+confidence interval to have a 90% chance of being smaller than ±0.05 %/year
+(results for other thresholds can be viewed using the layer control in
+the corner of the map).
+
+.. map-widget:: 
+   :colorscale_min: 2
+   :colorscale_max: 10
+   :colorscale_name: Spectral
+   :short_description: Data Length [years]
+   :layers_title: PLR Uncertainty:
+
+    synthetic-plr/can275_0.1_90.tiff : ±0.05 %/year
+    synthetic-plr/can275_0.2_90.tiff : ±0.1 %/year
+
+
+This map shows that fewer years of data are needed in the desert southwest
+than elsewhere in the country to achieve a given uncertainty level.
+However, relaxing the required certainty level reduces the required dataset
+length, with almost everywhere in the U.S. requiring only 2-3 years of data
+to achieve PLR estimates of ±0.1 %/year (under the idealized conditions
+described above).
+
+
+Scenario 2: CdTe
+----------------
+
+Do these results depend significantly on the technology being examined?
+The following maps show the same analysis as above, but assuming First Solar
+Cadmium Telluride (CdTe) PV modules instead of crystalline silicon.
+
+
+.. map-widget:: 
+   :colorscale_min: 2
+   :colorscale_max: 10
+   :colorscale_name: Spectral
+   :short_description: Data Length [years]
+   :layers_title: PLR Uncertainty:
+
+    synthetic-plr/fslr_0.1_90.tiff : ±0.05 %/year
+    synthetic-plr/fslr_0.2_90.tiff : ±0.1 %/year
+
+
+The results in this case show broadly the same pattern as in the c-Si map,
+although any given location might require one more or one fewer year of data.
+
+
+References
+----------
+
+.. bibliography::
+   :list: enumerated
+   :filter: False 
+
+   Theristis2023srrl
+   Deceglie2023
+   Jordan2022
+   
+
+Data files
+----------
+
+The geographic datasets shown on this page are available in the GeoTIFF
+files listed below:
+
+.. geotiff-index::
+    :pattern: geotiffs/synthetic-plr/*.tiff
+
---
+++ website/source/conf.py
@@ -0,0 +1,266 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+repo_url = "https://github.com/kandersolar/pvatlas"
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PV Atlas"
+copyright = '2023-2024'
+author = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinxcontrib.bibtex',
+    'sphinx.ext.extlinks',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# https://sphinxcontrib-bibtex.readthedocs.io/en/latest/usage.html#configuration
+bibtex_default_style = 'plain'
+bibtex_reference_style = 'label'
+bibtex_bibfiles = ['references.bib']
+
+bibtex_cite_id = "cite-{bibliography_count}-{key}"
+bibtex_footcite_id = "footcite-{key}"
+bibtex_bibliography_id = "bibliography-{bibliography_count}"
+bibtex_footbibliography_id = "footbibliography-{footbibliography_count}"
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_title = "PV Atlas"
+
+html_theme_options = {
+    "repository_url": repo_url,
+    "use_repository_button": True,
+}
+
+html_extra_path = ['../../data']
+
+
+extlinks = {
+    'script': (repo_url + '/tree/main/code/simulation-scripts/%s', ''),
+}
+
+
+# %%
+
+from docutils.parsers.rst import Directive, directives
+from docutils import nodes
+
+import glob
+import os
+import json
+import rioxarray
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if os.getenv("GITHUB_ACTIONS") == "true":
+    HTML_ROOT = "/pvatlas/"
+else:
+    HTML_ROOT = "/"
+
+
+with open("_static/map-header.html", "r") as f:
+    MAP_HTML_HEADER = f.read()
+    MAP_HTML_HEADER = MAP_HTML_HEADER.replace("##HTML_ROOT##", HTML_ROOT)
+
+with open("_static/map-widget-template.html", "r") as f:
+    MAP_HTML_TEMPLATE = f.read()
+
+
+class MapHeader(Directive):
+
+    required_arguments = 0
+    optional_arguments = 0
+    has_content = False
+    option_spec = {}
+
+    def run(self):
+        node = nodes.raw('', MAP_HTML_HEADER, format='html')
+        return [node]
+
+
+class MapWidget(Directive):
+
+    required_arguments = 0
+    optional_arguments = 0
+    has_content = True
+    option_spec = {
+        'colorscale_min' : float,
+        'colorscale_max': float,
+        'colorscale_name': str,
+        'colorscale_label_digits': int,
+        'short_description': str,
+        'layers_title': str,
+    }
+
+    def _parse_layer_specs(self, raw_specs):
+        specs = []
+        for raw_spec in raw_specs:
+            if ":" in raw_spec:
+                filename, label = raw_spec.split(":", 1)
+            else:
+                # if no label supplied, fall back to just using the filename
+                filename = label = raw_spec
+
+            label = label.strip()
+            filename = filename.strip()
+            uri = HTML_ROOT + "geotiffs/" + filename
+            specs.append({
+                'label': label,
+                'filename': uri,
+            })
+        return specs
+
+
+    def run(self):
+        
+        raw_specs = filter(lambda s: s != '', self.content)
+        specs = self._parse_layer_specs(raw_specs)
+        
+        uuid = self.state.document.settings.env.new_serialno('map-widget')
+        # TODO: use jinja instead of this hacky templating
+        options = {
+            'id': str(uuid),
+            'geotiffSpecs': specs,
+        }
+        defaults = {
+            'colorscale_min': '',
+            'colorscale_max': '',
+            'colorscale_name': '',
+            'colorscale_label_digits': 1,
+            'short_description': 'Value',
+            'layers_title': '',
+        }
+        for key, value in defaults.items():
+            options[key] = self.options.get(key, value)
+        
+        html = (
+            MAP_HTML_TEMPLATE
+            .replace("##OPTIONS##", json.dumps(options))
+            .replace("##ID##", options['id'])
+        )
+        node = nodes.raw('', html, format='html')
+        return [node]
+
+
+def _filesize_format(n_bytes):
+    n = n_bytes
+    for unit in ['B', 'KiB', 'MiB']:
+        if n < 1024:
+            break
+        n /= 1024
+    else:
+        unit = 'GiB'
+    return f"{n:0.01f} {unit}"
+
+
+class GeotiffIndex(Directive):
+    required_arguments = 0
+    optional_arguments = 0
+    has_content = False
+    option_spec = {'pattern': directives.unchanged}
+
+    def run(self):
+        env = self.state.document.settings.env
+    
+        images_directory = os.path.normpath("./source/_static/thumbnails")
+        print(f"pv atlas: creating thumbnail directory ({images_directory})")
+        os.makedirs(images_directory, exist_ok=True)
+        
+        DATA_DIR = os.path.abspath('../data')
+        pattern = self.options.get('pattern', '**/*.tiff')
+        print(f"pv atlas: looking for GeoTIFF files in {DATA_DIR} using pattern: {pattern}")
+        filenames = sorted(glob.glob(pattern, root_dir=DATA_DIR, recursive=True))
+        print(f"pv atlas: found {len(filenames)} GeoTIFF files")
+        
+        records = []
+        for filename in filenames:
+            print(f"pv atlas: processing {filename}")
+            rds = rioxarray.open_rasterio(os.path.join(DATA_DIR, filename))
+            rds = rds.sel(band=1)  # select first (only) layer of data
+            rds = rds.where(rds != rds.attrs['_FillValue'], np.nan)
+
+            image_filepath = os.path.join(images_directory, filename.replace('.tiff', '.png'))
+            env.images.add_file('', image_filepath)
+
+            if not os.path.exists(image_filepath):
+                os.makedirs(os.path.dirname(image_filepath), exist_ok=True)
+
+                rds.plot(add_colorbar=False, size=3, aspect=1.5)
+                plt.axis('off')
+                plt.title(None)
+                plt.savefig(image_filepath, bbox_inches='tight', dpi=100)
+                plt.close('all')
+                print(f"pv atlas: saved thumbnail to {image_filepath}")
+
+            records.append({
+                'description': rds.attrs.get('DESCRIPTION', '-'),
+                'date': rds.attrs.get('CREATION_DATE', '-'),
+                'displayname': os.path.split(filename)[-1],
+                'url': HTML_ROOT + filename.replace(os.sep, '/'),  # location within the data directory
+                'filesize': _filesize_format(os.path.getsize(os.path.join(DATA_DIR, filename))),
+                'thumbnail': "/" + image_filepath.replace("source" + os.sep, ""),
+            })
+
+            
+        print("pv atlas: parsed GeoTIFF metadata:")
+        print(pd.DataFrame(records))
+        
+        table = nodes.table(cols=3)
+        group = nodes.tgroup()
+        head = nodes.thead()
+        body = nodes.tbody()
+        
+        table += group
+        group += nodes.colspec(colwidth=3)
+        group += nodes.colspec(colwidth=8)
+        group += head
+        group += body
+        
+        row = nodes.row()
+        row += nodes.entry('', nodes.paragraph('', nodes.Text('GeoTIFF file')))
+        row += nodes.entry('', nodes.paragraph('', nodes.Text('Description')))
+        head += row
+        
+        for record in records:
+            row = nodes.row()            
+            
+            thumbnailnode = nodes.paragraph()
+            linknode = nodes.reference('', '', internal=False, refuri=record['url'])
+            linknode.append(nodes.image("", uri=record['thumbnail'], alt=record['thumbnail']))
+            thumbnailnode += linknode
+            row += nodes.entry('', thumbnailnode)
+            
+            description = nodes.paragraph('', nodes.Text("Description: " + record['description']))
+            downloadinfo = nodes.paragraph()
+            linknode = nodes.reference('', '', internal=False, refuri=record['url'])
+            linknode.append(nodes.raw("", '<i class="fa fa-download" aria-hidden="true"></i>&nbsp;', format="html"))
+            linknode.append(nodes.Text(record['displayname'] + " - [" + record['filesize'] + "]"))
+            downloadinfo += linknode
+            description.append(downloadinfo)
+            row += nodes.entry('', description)
+            
+            body += row
+
+        return [table]
+
+
+def setup(app):
+    app.add_directive('map-widget', MapWidget)
+    app.add_directive('map-header', MapHeader)
+    app.add_directive('geotiff-index', GeotiffIndex)
